51eba08ac7845923e1130dd8c353cbc9ace08785
Nashath Rasha <nashathrasha0@gmail.com>
2025-12-29 02:14:01 +0530

    design to blade initial commit

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/7 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>Python Programmer</h3>
+        <p>Part time-80k LKR</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        <img class="w-12 h-12" src="https://placekitten.com/100/100" alt="" class="rounded-xl">
+    </div>
+</div>
